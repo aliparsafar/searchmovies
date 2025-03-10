e0ed722910b9e656e65771094ffb1ef8852aa8f5
aliparsafar <aliparsafar.code@gmail.com>
2025-03-10 19:05:12 +0330

    edit app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import SearchMovies from "./components/searchmovies";
 
 function App() {
   return (
     <div className="container mx-auto">
-      
-     
+      <h1 className="text-2xl font-bold text-center my-4">Movie Search</h1>
+      <Routes>
+        <Route path="/" element={<SearchMovies />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
 
