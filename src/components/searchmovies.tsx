41d26dc15baed27547f82869a4a3530080c94336
aliparsafar <aliparsafar.code@gmail.com>
2025-03-10 18:59:20 +0330

    edit searchmovies component

--- src/components/searchmovies.tsx
+++ src/components/searchmovies.tsx
@@ -1,1 +1,11 @@
+import axios from "axios";
 
+
+
+const fetchMovies = async (searchTerm: string) => {
+  if (!searchTerm) return [];
+  const { data } = await axios.get(
+    `https://www.omdbapi.com/?s=${searchTerm}&apikey=${API_KEY}`
+  );
+  return data.Search || [];
+};
